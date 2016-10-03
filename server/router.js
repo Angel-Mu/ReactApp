02127ac72ceb76ae62dcea6ee9fdf3367febd125
Angel Malavar <angel.malavar@gmail.com>
2016-10-03 12:24:28 -0500

    Correct redirect *

--- server/router.js
+++ server/router.js
@@ -11,8 +11,8 @@ module.exports = function(app) {
 
 	app.use('/api', apiRoutes);
 
-	// app.get('*', (req, res) => {
-	//   res.sendFile(path.resolve(__dirname, '../client/index.html'));
-	// });
+	app.get('*', (req, res) => {
+	  res.sendFile(path.resolve(__dirname, '../client/index.html'));
+	});
 
 }
